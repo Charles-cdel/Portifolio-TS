a78783ec990acce6e3fd479bb5b16c708935f0a8
Carlos Alberto Carneiro <lionbllack31@gmail.com>
2024-08-24 22:03:07 -0300

    ajustando botoes

--- src/pages/Home/Sections/Hero/Hero.tsx
+++ src/pages/Home/Sections/Hero/Hero.tsx
@@ -1,21 +1,25 @@
-import { Button, Container, Grid, styled, Typography } from "@mui/material"
+import {Container, Grid, styled, Typography } from "@mui/material"
 import FotoPerfil from "../../../../assets/images/FotoPerfil.jpeg"
 import DownloadIcon from '@mui/icons-material/Download';
 import EmailIcon from '@mui/icons-material/Email';
+import StyledButton from "../../../../Components/StyledButton/StyledButton";
 
 
 const Hero = () => {
 
   const StyledHero = styled("div")(({theme})=>({
     backgroundColor: theme.palette.primary.main,
-    height: "100vh"
+    height: "100vh",
+    display: "flex",
+    alignItems:"center"
 
 
   }))
 
-  const StyledImg = styled("img")(()=>({
-    width: "100%",
-    borderRadius: "50%"
+  const StyledImg = styled("img")(({theme})=>({
+    width: "80%",
+    borderRadius: "50%",
+    border:`solid 1px ${theme.palette.primary.contrastText}`
 
 
 
@@ -29,40 +33,51 @@ const Hero = () => {
 
         <Grid container spacing={2}> 
 
-        <Grid iten xs={12} md={4}>
+        <Grid iten xs={12} md={5}>
 
         <StyledImg src= {FotoPerfil}/>
 
         </Grid>
 
-        <Grid iten xs={12} md={8}>
+        <Grid iten xs={12} md={7}>
 
           <Typography color="primary.contrastText" variant="h1" textAlign="center"> Carlos Alberto Carneiro </Typography>
           <Typography color="primary.contrastText" variant="h2" textAlign="center"> Sou Desenvolvedor FullStack </Typography>
 
-          <Grid container display="flex" justifyContent="center">
+          <Grid container display="flex" justifyContent="center" gap={3}>
 
             <Grid iten xs={12} md={4} display="flex" justifyContent="center">
 
-              <Button>
+              
+              <StyledButton>
+
+              
 
               <DownloadIcon/>
 
+              <Typography>
               Baixar CV
 
-              </Button>
+              </Typography>
+
+              
+
+              
+              </StyledButton>
 
             </Grid>
 
             <Grid iten xs={12} md={4} display="flex" justifyContent="center">
 
-            <Button>
+            <StyledButton>
 
             <EmailIcon/>
             
+            <Typography>
             Contate-me
+            </Typography>
 
-            </Button>
+            </StyledButton>
               
             </Grid>
           </Grid>
